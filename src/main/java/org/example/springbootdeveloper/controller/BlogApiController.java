32f38584dbaba8b9885aaba72185c702cbad3d46
changhoonYoo <kuilcake@gmail.com>
2025-01-07 17:48:51 +0900

    20250107: 7장 완료

--- src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
-//@Controller
 @RestController
 public class BlogApiController {
 
--- src/main/java/org/example/springbootdeveloper/domain/Article.java
+++ src/main/java/org/example/springbootdeveloper/domain/Article.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @AllArgsConstructor
 @Getter
 public class Article {
