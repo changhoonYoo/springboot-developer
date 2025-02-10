b285118df7fc96fa2270a1f13b54edccb464011a
changhoonYoo <kuilcake@gmail.com>
2025-02-10 17:35:46 +0900

    20250210: article author 추가, 로그인 엔드포인트 수정, 뷰 작업중

--- src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,8 +24,8 @@ public class BlogApiController {
 
     @PostMapping("/api/articles")
     @Operation(summary = "Add Article", description = "블로그 글 추가")
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
-        Article savedArticle = blogService.save(request);
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request, Principal principal) {
+        Article savedArticle = blogService.save(request, principal.getName());
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
